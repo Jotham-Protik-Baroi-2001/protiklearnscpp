13342ba381722e3b61e6147dd0251da9003694de
Jotham-Protik-Baroi-2001 <jotham.protik.baroi.mail@gmail.com>
2025-09-04 20:06:46 +0600

    First day, new things

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,15 @@ int main(){
     long long var1;
     long long var2;
     long long var3;
+    int values[]={1,2,3,4,5,6,7,8,9};
+    int length = sizeof(values)/sizeof(values[0]);
+int counter=0;
+while(counter<length-1){
+std::cout<<values[counter]<<" "<<std::endl;
+counter++;
+
+
+}
 
 std::cout<<"Hello world!\n Please enter two values to calculate"<<std::endl;
     cin>>var1;
@@ -26,3 +35,9 @@ int backup(long long var3) {
     std::cout<<var3<<" Hoise value bodda"<<std::endl;
     return 0;
 }
+
+
+
+
+
+
